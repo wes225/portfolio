01635b489984bdad30f59fafb8354b0a13dd42cf
wes225 <hossi225@hotmail.com>
2018-05-07 19:07:52 -0400

    Enables responsive portfolio. Adds touch events for mobile users.

--- index.js
+++ index.js
@@ -3,44 +3,11 @@ window.onload = function () {
     let portfolioWebsites = document.getElementsByClassName('overlay');
     let overlays = document.getElementsByClassName('hoverDisplay');
 
-    // Mobile support for hover effect will be implemented at a later date. Function
-    // checks if mobile, and if so, eventlisteners will not be implemented.
-    var isMobile = {
-        Android: function () {
-            return navigator
-                .userAgent
-                .match(/Android/i);
-        },
-        BlackBerry: function () {
-            return navigator
-                .userAgent
-                .match(/BlackBerry/i);
-        },
-        iOS: function () {
-            return navigator
-                .userAgent
-                .match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function () {
-            return navigator
-                .userAgent
-                .match(/Opera Mini/i);
-        },
-        Windows: function () {
-            return navigator
-                .userAgent
-                .match(/IEMobile/i);
-        },
-        any: function () {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-    };
-
     for (let i = 0; i < portfolioWebsites.length; i++) {
 
-        if (!isMobile.any()) {
+        
 
-            // Handles hover events
+            // Handles start of hover/touch events
             portfolioWebsites[i]
                 .addEventListener("mouseenter", function () {
 
@@ -49,13 +16,26 @@ window.onload = function () {
                         .add('iconCentered');
                 });
 
-            // Handles end of hover effects
+                portfolioWebsites[i]
+                .addEventListener("touchstart", function () {
+
+                    overlays[i]
+                        .classList
+                        .add('iconCentered');
+                });
+
+            // Handles end of hover and touch events
             portfolioWebsites[i].addEventListener("mouseleave", function () {
                 overlays[i]
                     .classList
                     .remove('iconCentered');
             })
-        }
+            portfolioWebsites[i].addEventListener("touchend", function () {
+                overlays[i]
+                    .classList
+                    .remove('iconCentered');
+            })
+        
     }
 
 }
