3efae0455233d38c70b49c832398c45c8592c51d
Ouissem Hammami <hossi225@hotmail.fr>
2018-08-11 17:08:15 -0400

    Adds content to Homepage and About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,33 @@ import Link from 'gatsby-link'
 
 const About = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>ABOUT</h1>
+    <p>I currently work as web integrator for Desjardins. Before that, I worked a
+      lot in QA.</p>
+      <p>
+      I graduated from a college degree in Mobile development, then taught myself
+      front end development.
+      </p>
+      <p>
+      My favorite stack at the moment is React, and I'm slowly learning about Node and
+      GraphQL. I have worked with Webpack, Firebase, ReactRouter and now Gatsby (this
+      portfolio is build on it!)
+      </p><p>
+      When I'm not working I'm trying to find a new cool restaurent, or looking at
+      crazy expensive coffee machines. Sometimes taking walks or at the Gym.
+      </p><p>
+      Oh, I'm also creating a boardgame on the side.
+      </p>
+      <p>
+      That should cover it. Or some of it. If you need help with your projects, get in
+      touch {< Link
+      to = "/contact"
+      style = {{
+          textDecoration: 'none'
+        }} > here </Link>}
+
+    </p>
+
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,12 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi, I'm Ouissem</h1>
+    <p>I like coding web things.</p>
+    <p>Especially in Javascript. But mostly front-end at the moment</p>
+    <br/>
+    <p>{'Try to find some of the interactions on the page :)'} </p>
+
    
   </div>
 )
