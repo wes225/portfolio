c0eac8359af9f4d2c41930d91bfb7b07aa72767c
Ouissem Hammami <hossi225@hotmail.fr>
2018-08-11 11:59:15 -0400

    Adds Sass and Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,18 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-sass`, {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+      // Puts tracking script in the head instead of the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+      // Avoids sending pageview hits from custom paths
+      exclude: ["/preview/**", "/do-not-track/me/too/"],
+    },
+  }],
 }
