1714f1cd47aea882b17fd4fb99d9769edf2bfe85
Ouissem Hammami <hossi225@hotmail.fr>
2018-08-22 20:06:05 -0400

    Prepares for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,35 @@
 module.exports = {
   siteMetadata: {
     title: 'Ouissem Hammami',
-  },
-plugins : [
-  'gatsby-plugin-react-helmet',
-  `gatsby-plugin-sass`,
-  `gatsby-plugin-typography`,
-  `gatsby-transformer-json`, {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `data`,
-          path: `${__dirname}/src/data/`,
-        },
-      } ,{
-    resolve: `gatsby-plugin-typography`,
-    options: {
-      pathToConfigModule: `src/utils/typography.js`,
-    },
-  }, {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-      trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-      // Puts tracking script in the head instead of the body
-      head: false,
-      // Setting this parameter is optional
-      anonymize: true,
-      // Setting this parameter is also optional
-      respectDNT: true,
-      // Avoids sending pageview hits from custom paths
-      exclude: ["/preview/**", "/do-not-track/me/too/"],
-    },
-  }],
+  }, pathPrefix: "portfolio",
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-typography`,
+    `gatsby-transformer-json`, {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    }, {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    }, {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,8 @@ const IndexPage = () => (
     <p>I like coding web things.</p>
     <p>Especially in Javascript. But mostly front-end at the moment</p>
     <br/>
-    <p>{'Try to find some of the interactions on the page :)'} </p>
+    <del>{"Try to find some of the interactions on the page :)"} </del>
+    <p>I haven't actually implemented them yet. Not much CSS either actually. But it's getting there!</p>
 
    
   </div>
