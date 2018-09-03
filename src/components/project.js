a2c145e0c1ee31e69922e8be73cd1d2b80093c0a
Ouissem Hammami <hossi225@hotmail.fr>
2018-09-03 16:18:01 -0400

    Adds project styling and layout

--- src/components/project.js
+++ src/components/project.js
@@ -1,26 +1,56 @@
 import React from 'react'
+import styled from 'react-emotion'
 
-const style = {
-  wrapper: {
-    border: "1 px black`"
-  },
-  link: {
-    textDecoration: "none"
+const Box = styled('a')`
+& {
+  transition: all 0.3s 0.2s ease;
+  align-self: flex-start;
+  margin:10px;
+  text-decoration:none;
+  width:280px;
+  height:300px;
+  padding:15px 10px 10px 10px;
+  box-shadow:0 0 5px rgba(0, 0, 0, 0.5);
 
+  :hover{
+    transition: all 0.3s 0.2s ease;
+    box-shadow:14px 14px 8px rgba(0, 0, 0, 0.8);
+    background-color:rgba(0, 0, 0, 0.9);
+    h2{
+    transition: all 0.3s 0.2s ease;
+    color:rgba(175, 166, 166, 1);
+    }
+    p{
+    transition: all 0.3s 0.2s ease;
+    color:rgba(175, 166, 166, 1);}
+    }
   }
 
-}
+
+  h2{
+    transition: all 0.3s 0.2s ease;
+    font-size: "18px";
+    line-height: 1.45;
+    color:black;
+  }
+  p{
+    transition: all 0.3s 0.2s ease;
+    font-size: "18px";
+    line-height: 1.45;
+    color:black;}
+  }
+`
 
 const Project = ({
   ...props
 }) => {
   return (
-    <div style={style.wrapper}>
-      <a href={props.liveURL} target="_blank" style={style.link}>
+    
+      <Box href={props.liveURL} target="_blank">
         <h2>{props.name}</h2>
         <p>{props.description}</p>
-      </a>
-    </div>
+      </Box>
+    
   )
 }
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,13 +2,25 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Project from '../components/Project'
 import PageTransition from 'gatsby-plugin-page-transitions';
+import styled from 'react-emotion'
 
+const ProjectWrapper = styled('div')`
+& {
+  display:flex;
+ flex-flow:row;
+flex-wrap: wrap;
+justify-content:space-between;
+align-items:flex-start;
+padding:5px;
+margin:5px;
+  }
+`
 
 const Projects = ({data}) => (
   <PageTransition>
     <h2>These are the projects I worked on.</h2>
     <h3>Sorted by newest</h3>
-    <div>{data.dataJson.projects.map((data,index) =>(<Project key={index} {...data}></Project>))}</div>
+    <ProjectWrapper>{data.dataJson.projects.map((data,index) =>(<Project key={index} {...data}></Project>))}</ProjectWrapper>
     
   </PageTransition>
 )
