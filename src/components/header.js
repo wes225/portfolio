d0f19910bf886c7d7207b23a604cc7b6d1a5a1bc
Ouissem Hammami <hossi225@hotmail.fr>
2018-09-03 13:14:24 -0400

    Adds animation de menu buttons.

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,135 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'react-emotion'
+
+const Label = styled('label')`
+
+& {
+  padding: 10px 25px;
+  background: transparent;
+  outline: none !important;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  width:120px;
+  line-height: 50px;
+  padding: 0;
+  border: none;
+  
+}
+& span {
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 100%;
+  text-align:center;
+}
+&::before, &::after {
+  position: absolute;
+  content: "";
+  height: 0%;
+  width: 2px;
+  background: #000;
+}
+&::before {
+  right: 0;
+  top: 0;
+  transition: all 0.1s ease;
+}
+&::after {
+  left: 0;
+  bottom: 0;
+  transition: all 0.1s 0.2s ease;
+}
+&:hover:before {
+  transition: all 0.1s 0.2s ease;
+  height: 100%;
+}
+&:hover:after {
+  transition: all 0.1s ease;
+  height: 100%;
+}
+& span:before,
+& span:after {
+  position: absolute;
+  content: "";
+  background: #000;
+}
+& span:before {
+  left: 0;
+  top: 0;
+  width: 0%;
+  height: 2px;
+  transition: all 0.1s 0.1s ease;
+}
+& span:after {
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 2px;
+}
+& span:hover:before {
+  width: 100%;
+}
+
+`
+const Navbar = styled('div')`
+padding:2% 10% 5% 10%;
+display:flex;
+flex-flow:row;
+flex-wrap: wrap;
+justify-content:space-evenly;
+
+
+`
 
 const Header = ({siteTitle}) => (
   <div style={{
-    
     marginBottom: '1.45rem'
   }}>
-    <div
-      style={{
-      margin: '0 auto',
-      maxWidth: 960,
-      padding: '1.45rem 1.0875rem'
-    }}>
-      <h1 style={{
-        margin: 0
+    <Navbar>
+
+      <Link to="/" style={{
+        textDecoration: 'none',
+        flex: '0 1 100px'
       }}>
-        <Link
-          to="/"
-          style={{
-          textDecoration: 'none',
-          paddingRight: 20
-        }}>Home</Link>
-        <Link
-          to="/about"
-          style={{
-          textDecoration: 'none',
-          paddingRight: 20
-        }}>About</Link>
-        <Link
-          to="/projects"
-          style={{
-          textDecoration: 'none',
-          paddingRight: 20
-        }}>Projects</Link>
-        <Link
-          to="/contact"
-          style={{
-          textDecoration: 'none',
-          paddingRight: 20
-        }}>Contact</Link>
-      </h1>
-    </div>
+        <Label>
+          <span>Home</span>
+        </Label>
+      </Link>
+      <Link
+        to="/about"
+        style={{
+        textDecoration: 'none',
+        flex: '0 1 100px'
+      }}>
+        <Label>
+          <span>About</span>
+        </Label>
+      </Link>
+      <Link
+        to="/projects"
+        style={{
+        textDecoration: 'none',
+        flex: '0 1 100px'
+      }}>
+        <Label>
+          <span>Projects</span>
+        </Label>
+      </Link>
+      <Link
+        to="/contact"
+        style={{
+        textDecoration: 'none',
+        flex: '0 1 100px'
+      }}>
+        <Label>
+          <span>Contact</span>
+        </Label>
+      </Link>
+
+    </Navbar>
 
   </div>
 )
